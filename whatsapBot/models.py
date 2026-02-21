a3bb6e4949604cc14d23772f6cd05eff5dabda46
Gaji Yaqub <gajiyakub6@gmail.com>
2026-02-21 18:49:45 +0100

    Fixed Trying to get users from frontend

--- whatsapBot/models.py
+++ whatsapBot/models.py
@@ -8,7 +8,7 @@ class Message(Base):
     __tablename__ = "messages"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    user_id = Column(Integer,  nullable=False)
     phone_number = Column(String, nullable=False)
     token = Column(UUID(as_uuid=True), default=uuid.uuid4, unique=True)
     role = Column(String, nullable=False)
